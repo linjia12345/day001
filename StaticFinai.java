b7c54b9eb11e368772e9beed50d9ceb9a4e5e623
linjiateng123 <624003294@qq.com>
2017-12-01 16:46:36 +0800

    第一次提交

---
+++ StaticFinai.java
@@ -0,0 +1,14 @@
+package day09;
+
+public class StaticFinai {
+
+	public static void main(String[] args) {
+		System.out.println(Aoo.PI);
+
+	}
+}
+       class Aoo{
+    	   public static final double PI=3.14159;
+    	   public static final int a=1;
+    	   
+       }
